27e6fc8b17c2d3ef539838a171705048a8610386
Bùi Văn Toán <buivantoanbg2001@gmail.com>
2022-11-22 18:44:36 +0700

    Create UI Login, restruct project

--- convershark-web/src/App.tsx
+++ convershark-web/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import AppRouting from './components/routes/AppRouting';
+import { history, HistoryRouter } from './components/routes/HistoryRouter';
+import 'styles/main.scss';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HistoryRouter history={history}>
+      <Suspense fallback={null}>
+        <AppRouting />
+      </Suspense>
+    </HistoryRouter>
   );
-}
+};
 
 export default App;
