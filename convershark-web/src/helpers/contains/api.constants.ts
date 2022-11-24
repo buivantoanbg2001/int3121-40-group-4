d2deaf98eb87477b93568e6a0f202a000605032e
Bùi Văn Toán <buivantoanbg2001@gmail.com>
2022-11-24 15:41:14 +0700

    config .env, setup model schema

---
+++ convershark-web/src/helpers/contains/api.constants.ts
@@ -0,0 +1,9 @@
+const DOMAIN_API = `${process.env.REACT_APP_DOMAIN_API}`;
+
+export const AxiosClientConfig = {
+  DOMAIN_API: DOMAIN_API,
+  AUTH_TYPES: 'Bearer',
+  CONTENT_TYPE: 'application/json',
+};
+
+export const API = {};
---
+++ convershark-web/src/helpers/contains/constants.ts
@@ -0,0 +1,2 @@
+export const LOGO_URL =
+  'https://user-images.githubusercontent.com/62609188/203345422-1a7607bf-2082-47e9-ada6-9661a6319581.png';
