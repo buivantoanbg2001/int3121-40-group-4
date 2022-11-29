331b457693d11508642489bef923063e2629e50c
Bùi Văn Toán <buivantoanbg2001@gmail.com>
2022-11-30 01:15:42 +0700

    add api users/friends/update-both

--- convershark-api/src/main.ts
+++ convershark-api/src/main.ts
@@ -7,17 +7,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  
+
   const config = new DocumentBuilder()
     .setTitle('Convershark')
-    .setDescription('The Convershark-API description.')
+    .setDescription('The Convershark API documentation.')
     .setVersion('1.0')
     .addTag('')
+    .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-
   await app.listen(process.env.PORT);
+
+  console.log(`Listening on port ${process.env.PORT}`);
 }
 bootstrap();
